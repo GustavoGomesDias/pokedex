5b3b2c4c257b7fc039ffee24b93d95a229d79302
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-04 19:47:13 -0300

    Aula 282 - Butão de mudar imagem (front/back)

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -3,7 +3,7 @@
         <div class="card">
             <div class="card-image">
                 <figure>
-                    <img :src="pokemon.front" alt="Placeholder image">
+                    <img :src="currentImg" alt="Placeholder image">
                 </figure>
             </div>
             <div class="card-content">
@@ -15,7 +15,7 @@
                 </div>
 
                 <div class="content">
-                    
+                    <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar Sprite</button>
                 </div>
             </div>
         </div>
@@ -33,12 +33,14 @@ export default {
                 this.pokemon.type = res.data.types[0].type.name;
                 this.pokemon.front = res.data.sprites.front_default;
                 this.pokemon.back = res.data.sprites.back_default;
-
+                this.currentImg = this.pokemon.front;
                 console.log(this.pokemon);
             });
     },
     data(){
         return {
+            isFront: true,
+            currentImg: '',
             pokemon: {
                 type: '',
                 front: '',
@@ -56,6 +58,15 @@ export default {
         upper: function(value){
             const newName = value[0].toUpperCase() + value.slice(1);
             return newName;
+        },
+        mudarSprite: function(){
+            if(this.isFront){
+                this.isFront = false;
+                this.currentImg = this.pokemon.back;
+            }else{
+                this.isFront = true;
+                this.currentImg = this.pokemon.front;
+            }
         }
     }
 }
