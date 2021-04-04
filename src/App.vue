a945ece90270ed2f78258d998512de0fc0aa347a
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-04 17:21:51 -0300

    Aula 277 - Setando o Bulma (para CSS)  e o Aixos (para consumo de API)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
