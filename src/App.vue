298dbb20edd2c73a78d87a3f11b1834d09120602
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-04 20:39:57 -0300

    Aula 284 - Sistema simples de busca

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="column is-half is-offset-one-quarter">
-    <div v-for="(pokemon, index) in pokemons" :key="index">
+    <h4 class="is-size-4">PokéDex</h4>
+    <input type="text" placeholder="Buscar pelo nome..." v-model="busca" class="input is-rounded">
+    <button class="button is-fullwidth is-success" id="busca-btn" @click="buscar">Buscar</button>
+    <div v-for="(pokemon, index) in filteredPokemons" :key="pokemon.url">
       <Pokemon 
         :name="pokemon.name"
         :url="pokemon.url"
@@ -18,7 +21,9 @@ export default {
   name: 'App',
   data(){
     return {
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ""
     }
   },
 
@@ -26,11 +31,34 @@ export default {
   created: function(){
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
       console.log("Pegou a lista de Pokémons");
     });
   },
   components: {
     Pokemon
+  },
+  methods: {
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name.toUpperCase() == this.busca.toUpperCase());
+      }
+    }
+  },
+  computed: {
+    /* Busca sem botão
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name.toUpperCase() == this.busca.toUpperCase());
+      }
+    }
+    */
+
   }
 }
 </script>
@@ -43,5 +71,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#busca-btn {
+  margin-top: 5px;
 }
 </style>
